3714f7cfa62f5148e77ce104a2c9bd82fb5e1c84
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-08-11 19:09:16 -0400

    comentamos archivo Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import PizzaNav from "./assets/components/Navbar";
 import Home from "./assets/components/Home";
 import Footer from "./assets/components/Footer";
+import Register from "./assets/components/Register"
 
 
 function App() {
   return (
     <>
       <PizzaNav />
-      <Home />
-      <Footer/>
+      {/* <Home /> */}
+      <Register/>
+      {/* <Footer/> */}
     </>
   );
 }
 
 export default App;
+
+
